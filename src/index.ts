5a0646588a897bcc073678f1d73ce47c41e0c238
Nadir Kerem Cetin <nadirkerem12@gmail.com>
2024-06-25 17:33:47 -0400

    complete part 4

--- src/index.ts
+++ src/index.ts
@@ -90,3 +90,34 @@ class NCycle<T> {
     }
   }
 }
+
+const testCycle1 = new NCycle<number>(1, 2, 3);
+testCycle1.print();
+testCycle1.printAll();
+
+const testCycle2 = new NCycle<string>('This', 'That', 4);
+testCycle2.print();
+testCycle2.printAll();
+
+const testCycle3 = new NCycle<string>('Make', 10, 4);
+testCycle3.print(4);
+testCycle3.printAll();
+
+const makes4 = ['Volkswagon', 'Tesla', 'Audi'];
+const models4 = ['Passat', 'Model X', 'A4'];
+const testCycle4 = new NCycle<string[]>(makes4, models4, 4);
+testCycle4.print(2);
+testCycle4.printAll();
+
+const makes5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const models5 = [1, 1, 2, 3, 5];
+const testCycle5 = new NCycle<number[]>(makes5, models5, 0);
+testCycle5.print(7);
+testCycle5.printAll();
+
+function add(x: number, y: number): number {
+  return x + y;
+}
+add(testCycle1.make, testCycle5.model[1]);
+// Error expected here
+add(testCycle2.make, testCycle4.model[1]);
